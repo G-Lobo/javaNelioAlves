459a11f260c0e855f1255d41c429f7f718bf7464
Lobo <gbrilobo@gmail.com>
2024-04-03 14:44:42 -0300

    Final da section 13 e seus exercicios

---
+++ Enumeracoes e composicao/exercicio 3/src/entities/Order.java
@@ -0,0 +1,90 @@
+package entities;
+
+import entities.enums.OrderStatus;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Order {
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+
+    /*Atributos basicos*/
+    private Date moment;
+
+    /*associações*/
+    private OrderStatus status;
+    private Costumer costumer;
+
+    private List<OrderItem> items = new ArrayList<>();
+
+    public Order(Date moment, OrderStatus status, Costumer costumer) {
+        this.moment = moment;
+        this.status = status;
+        this.costumer = costumer;
+    }
+
+    public Date getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Date moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public Costumer getCostumer() {
+        return costumer;
+    }
+
+    public void setCostumer(Costumer costumer) {
+        this.costumer = costumer;
+    }
+
+    public List<OrderItem> getItems() {
+        return items;
+    }
+
+    public void addItem(OrderItem item) {
+        items.add(item);
+    }
+
+    public void removeItem(OrderItem item) {
+        items.remove(item);
+    }
+
+    public Double total() {
+        double sum = 0.0;
+        for (OrderItem item : items) {
+            sum += item.subTotal();
+        }
+        return sum;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Order moment: ");
+        sb.append(sdf.format(moment) + "\n");
+        sb.append("Order status: ");
+        sb.append(status + "\n");
+        sb.append("Costumer: ");
+        sb.append(costumer + "\n");
+        sb.append("Order items:\n");
+        for (OrderItem item : items) {
+            sb.append(item + "\n");
+        }
+        sb.append("Total price: $");
+        sb.append(String.format("%.2f", total()));
+        return sb.toString();
+    }
+}
--- src/application/Program.java
+++ src/application/Program.java
@@ -4,5 +4,7 @@ package application;
 public class Program {
     public static void main(String[] args) {
 
-        }
+
+
+    }
 }
