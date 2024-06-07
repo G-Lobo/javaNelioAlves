c61fa32eaa3256d96a665f7a24e835f55ede68ae
Lobo <gbrilobo@gmail.com>
2024-06-07 10:26:20 -0300

    fim da section 17 trabalhando com arquivos

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,41 +1,68 @@
 package application;
 
-
-
-import model.entities.Account;
-import model.exceptions.BusinessException;
-
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Product;
+
 public class Program {
-    public static void main(String[] args){
+
+    public static void main(String[] args) throws ParseException {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter Account data");
-        System.out.print("Account number: ");
-        int accNumber = sc.nextInt();
-        System.out.print("Holder Name: ");
-        sc.nextLine();
-        String holderName = sc.nextLine();
-        System.out.print("Initial Balance: ");
-        Double initialBalance = sc.nextDouble();
-        System.out.print("Withdraw limit: ");
-        double withdrawLimit = sc.nextDouble();
+        List<Product> list = new ArrayList<>();
 
-        Account account = new Account(accNumber, holderName, initialBalance, withdrawLimit);
+        System.out.println("Enter file path: ");
+        String sourceFileStr = sc.nextLine();
 
-        System.out.println();
-        System.out.print("Enter amount to withdraw: ");
-        Double withdrawAmount = sc.nextDouble();
+        File sourceFile = new File(sourceFileStr);
+        String sourceFolderStr = sourceFile.getParent();
 
-        try {
-        account.withdraw(withdrawAmount);
-        System.out.printf("New balance: %.2f%n", account.getBalance());
-        } catch (BusinessException e) {
-            System.out.println(e.getMessage());
+        boolean success = new File(sourceFolderStr + "\\out").mkdir();
+
+        String targetFileStr = sourceFolderStr + "\\out\\summary.csv";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(sourceFileStr))) {
+
+            String itemCsv = br.readLine();
+            while (itemCsv != null) {
+
+                String[] fields = itemCsv.split(",");
+                String name = fields[0];
+                double price = Double.parseDouble(fields[1]);
+                int quantity = Integer.parseInt(fields[2]);
+
+                list.add(new Product(name, price, quantity));
+
+                itemCsv = br.readLine();
+            }
+
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(targetFileStr))) {
+
+                for (Product item : list) {
+                    bw.write(item.getName() + "," + String.format("%.2f", item.total()));
+                    bw.newLine();
+                }
+
+                System.out.println(targetFileStr + " CREATED!");
+
+            } catch (IOException e) {
+                System.out.println("Error writing file: " + e.getMessage());
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
         }
 
         sc.close();
